8ebcd836fedf18183d4c1cad695b013cd8e8a3a3
mecastor <mecastor87@gmail.com>
2024-12-07 17:11:17 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ fn main() -> Result<()> {
     let _df = load_csv_file(OUTPUT_FILE)?;
 
     // Prepare the data by random splitting inro train and test sets
+    fn split_train_test(df: &DataFrame, test_size: f64) -> (DataFrame, DataFrame) {
+        let (train_df, test_df) = df.random_split(test_size);
+        (train_df, test_df)
+    }
 
     // Train an XGBoost model
     // Push to S3 bucket
