e38fdab807fe35e7c9cdd81becf8300e1d7fe07e
cian-oL <cianoleary2010@gmail.com>
2024-12-11 17:21:36 +0000

    api wip

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -1,4 +1,15 @@
-fn main() -> anyhow::Result<()> {
-    println!("Running api ");
-    Ok(())
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::io::Result;
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    HttpResponse::Ok().body("Health check OK")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().service(health))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
